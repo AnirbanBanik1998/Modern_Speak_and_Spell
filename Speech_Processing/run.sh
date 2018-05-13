cc35b53074ace761af2c1d2d1b8caf82a227e8fe
AnirbanBanik1998 <anirbanbanik17@gmail.com>
2018-05-14 01:22:32 +0530

    Updated run.sh

--- Speech_Processing/run.sh
+++ Speech_Processing/run.sh
@@ -1,9 +1,17 @@
+if [ -d wav ]
+then
+	echo "Directory wav already present"
+else
+	mkdir wav
+fi
 echo "Enter Sampling Rate:"
 read samples
 echo "Enter Chunk Size:"
 read chunk
 echo "Enter Minimum Volume:"
 read vol
+
+echo "Press CTRL+C to interrupt the recording"
 python3 record_new.py $samples $chunk $vol
 output= "$(ls wav | wc -l)"
 python3 edit.py $output
@@ -13,6 +21,8 @@ read lang
 echo "Enter Dictionary:"
 read dic
 
-pocketsphinx_batch -adcin yes -cepdir wav -cepext .wav -ctl test.fileids -lm $lang -dict $dic -samprate $samples -nfft $chunk -hyp test.hyp 
+pocketsphinx_batch -adcin yes -cepdir wav -cepext .wav -ctl test.fileids -lm $lang -dict $dic -samprate $samples -nfft $chunk -hyp test.hyp
+ 
+python3 word_align.py test.transcription test.hyp 
 output="$(perl word_align.pl test.transcription test.hyp)"
 python3 stat.py "$output"
