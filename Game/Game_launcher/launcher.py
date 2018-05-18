009d671ace5b8c132515e45d77338773cc24a388
AnirbanBanik1998 <anirbanbanik17@gmail.com>
2018-05-18 18:30:46 +0530

    Some Modifications

--- Game/Game_launcher/launcher.py
+++ Game/Game_launcher/launcher.py
@@ -5,33 +5,61 @@ lock=threading.Lock()
 f=open("./test.hyp","r")
 arr=f.read().split(" ")
 start=0
+resume=0
+quit=0
 for i in range(0, len(arr)):
 	if arr[i] in ("START", "start"):
 		start=1
 		break
+	elif arr[i] in ("RESUME", "resume"):
+		resume=1
+		break
+	elif arr[i] in ("QUIT", "quit"):
+		quit=1
+		break
 if start==1:
 	for i in range(0, len(arr)):
-		if arr[i] in ("SPELL", "spell", "RESUME", "resume"):
+		g=open("./previous.txt", "w+")
+		g.write(arr[i])
+		g.close()
+		if arr[i] in ("SPELL", "spell"):
 			with lock:
-				#print("Start Spell It!...")
 				cwd=os.getcwd()
-				os.chdir("../games/Spell_It!/")
+				os.chdir("../Games/Spell_It!/")
 				os.system("python3 Spell_It.py")
 				os.chdir(cwd)
 			break
+			'''
 		elif arr[i] in ("HANGMAN", "hangman"):
-			time.sleep(2)
-			with lock:
-				print("Start Hangman...")
+			#with lock:
+				#os.system("python3 ../Games/Hangman.py")
 			break
 		elif arr[i] in ("ENCRYPTER", "encrypter"):
-			time.sleep(2)
-			with lock:
-				print("Start Encrypter...")
+			#with lock:
+				#os.system("python3 ../Games/Encrypter.py")
 			break
 		elif arr[i] in ("CROSSWORD", "crossword"):
-			time.sleep(2)
-			with lock:
-				print("Start Crossword...")
+			#with lock:
+				#os.system("python3 ../Games/Crossword.py")
 			break
-
+			'''
+elif resume==1:
+	g=open("./previous.txt", "r+")
+	ar=g.read().split(" ")
+	if ar[0] in ("SPELL", "spell"):
+		with lock:
+			cwd=os.getcwd()
+			os.chdir("../Games/Spell_It!/")
+			os.system("python3 Spell_It.py")
+			os.chdir(cwd)
+			'''
+	#elif ar[0] in ("HANGMAN", "hangman"):
+		#with lock:
+			#os.system("python3 ../Games/Hangman/Hangman.py")
+	#elif ar[0] in ("ENCRYPTER", "encrypter"):
+		#with lock:
+			#os.system("python3 ../Games/Encrypter/Encrypter.py")
+	#elif ar[0] in ("CROSSWORD", "crossword"):
+		#with lock:
+			#os.system("python3 ../Games/Crossword/Crossword.py")
+	'''
