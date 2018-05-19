38c413a3ec91f070178727f12c7ce592c29f2fe7
AnirbanBanik1998 <anirbanbanik17@gmail.com>
2018-05-19 12:21:21 +0530

    Worked on voice based Hangman game

---
+++ Game/Games/Hangman/Hangman.py
@@ -0,0 +1,63 @@
+import random
+import subprocess
+import time
+import os
+import threading
+lock=threading.Lock()
+f= open("../Wordlist.csv",'r')
+rows=f.read().split("\n")
+k=0
+print("Enter one letter in a single try:")
+string=""
+str1=""
+my_randoms=random.sample(range(0, len(rows)), 1)
+column=rows[my_randoms[0]].split(",")
+randcolumn = random.sample(range(0, (len(column) - 1)), 1)
+column[randcolumn[0]]=column[randcolumn[0]].strip()
+for i in range(0, len(column[randcolumn[0]])):
+	string=string+"-"
+for i in range(0,len(string)):
+	str1=str1+string[i]+" "
+print (str1)
+		
+str1=""
+def check(str1,l):
+	print(" Input is "+l)
+	for j in range(0, len(string)):
+		if l==column[randcolumn[0]][j] and string[j]=="-":
+			str1=str1+l
+		else:
+			str1=str1+string[j]
+	return str1
+while k<20:
+	k=k+1
+	str1=""
+	letter=""
+	print("Enter letter: ")
+	with lock:
+		os.system("./input.sh")
+	r=open('./test.hyp','r')					
+	arr=r.read().split(" ")
+	letter=arr[0]
+	print(letter)
+	lt=letter.lower()				
+	try:
+		string=str(check(str1,lt))
+						
+	except:
+		print("Not Possible")
+			
+	str1=""
+	for j in range(0, len(string)):
+		str1=str1+string[j]+" "
+	print(str1)
+	if string==column[randcolumn[0]]:
+		subprocess.call(["espeak","You win"])
+		break
+	elif k==20:
+		subprocess.call(["espeak","You lose"])
+		with lock:
+			subprocess.call(["espeak","The answer is "+column[randcolumn[0]]])
+		print(" Answer is: "+column[randcolumn[0]])
+	
+subprocess.call(["espeak","-s","150"," Options are 1: Quit or 2: Resume"])
