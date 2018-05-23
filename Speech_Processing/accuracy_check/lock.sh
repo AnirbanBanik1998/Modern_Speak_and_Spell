4c0158f26169184431d4a8a21dcd25a7056cb296
AnirbanBanik1998 <anirbanbanik17@gmail.com>
2018-05-23 17:42:47 +0530

    Corrected accuracy_statistics files

--- Speech_Processing/accuracy_check/lock.sh
+++ Speech_Processing/accuracy_check/lock.sh
@@ -21,4 +21,4 @@ read dic
 pocketsphinx_batch -adcin yes -cepdir wav -cepext .wav -ctl test.fileids -lm $lang -dict $dic -samprate $1 -nfft $2 -hyp test.hyp
 output="$(perl word_align.pl test.transcription test.hyp)"
 python3 stat.py "$output"
-rm -rf "./wav/*"
+rm -rf ./wav/*
