2f7cf584758e1aa96c1c9ecce8bb398a6946ad6e
AnirbanBanik1998 <anirbanbanik17@gmail.com>
2018-05-29 23:55:39 +0530

    Updated methods to create phonetic dictionary based on frequencies of results in hypothesis files

---
+++ Speech_Processing/accuracy_check/add.py
@@ -0,0 +1,31 @@
+import shutil
+temp =open('temp.csv', 'w+')
+with open('phonetic.csv', 'r') as r:
+	for line in r:
+		words=line.split(",")
+		if len(words[0])>0:
+			f=open((words[0]+".dic"), "r")
+			lines=f.read().split("\n")
+			f.close()
+			for i in range(len(lines)):
+				arr=lines[i].split(" ")
+				string=""
+				if len(arr[0])>0 and int(arr[len(arr)-1])>0:
+					#dictionary.write(filename+"("+str(k)+")	"+ words[i]+"\n")
+					#k=k+1
+					for j in range(len(arr)-1):
+						string=string+arr[j]
+					if string not in words:
+						words.append(string)
+				
+				elif words[0] in arr[:]:
+					for j in range(len(arr)-1):
+						string=string+arr[j]
+					if string not in words:
+						words.append(string)
+			for j in range(len(words)):
+				temp.write(words[j]+",")
+			temp.write("\n")
+temp.close()
+shutil.move("temp.csv", "phonetic.csv")		
+				
