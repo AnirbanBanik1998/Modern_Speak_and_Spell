2f38ecdebb21608a05197b4003af57ddd7c9ca7c
AnirbanBanik1998 <anirbanbanik17@gmail.com>
2018-05-29 20:29:59 +0530

    Deleted the audio files...taking up too much space...sorry for the inconvenience

--- Speech_Processing/accuracy_check/dict.sh
+++ Speech_Processing/accuracy_check/dict.sh
@@ -1,14 +1,6 @@
-unzip samples.zip
-prev="./samples"
-new="."
-mv $prev/* $new
-rm -r "./samples"
 
 python3 dictionary.py
 python3 translate.py
 
 python3 final.py
 python3 make.py
-
-rm *.dic
-rm *.hyp
