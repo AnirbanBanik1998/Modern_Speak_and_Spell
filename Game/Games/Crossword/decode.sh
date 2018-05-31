e54e69bac26bc285d582275ba0fbfef2f46376cb
AnirbanBanik1998 <anirbanbanik17@gmail.com>
2018-06-01 00:15:05 +0530

    Added Side Scrips

---
+++ Game/Games/Crossword/decode.sh
@@ -0,0 +1,9 @@
+if [ -e test.fileids ]
+then
+	echo "test.fileids already present..."
+else
+	touch test.fileids
+fi
+
+python3 ../../edit.py 1
+pocketsphinx_batch -adcin yes -cepdir wav -cepext .wav -ctl test.fileids -lm ../../../Language_Models/characters.lm -dict ../../../Language_Models/characters.dic -samprate $1 -nfft $2 -hyp test.hyp
