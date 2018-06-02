8dc00cc42f20da9ff0c7f4dc4a4a31df1c8e7304
AnirbanBanik1998 <anirbanbanik17@gmail.com>
2018-06-02 11:04:10 +0530

    Updated maker.py

--- Game/Games/Crossword/maker.py
+++ Game/Games/Crossword/maker.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import time
 import subprocess
 import threading
@@ -16,18 +17,20 @@ mean = []
 for i in range(1,5):
 	mean.append(meaning(sys.argv[i]))
 
-def check(str1,l):
+def check(str1,l,w):
 	print(" Input is "+l)
-	for j in range(0, len(string)):
-		if l==column[randcolumn[0]][j] and string[j]=="-":
+	for j in range(4):
+		if l==w[j] and string[j]=="-":
 			str1=str1+l
 		else:
 			str1=str1+string[j]
 	return str1
+	
 def formation(w, q):
 	print("Meaning of the word is "+mean[q])
 	k=0
 	str1=""
+	global string
 	string=""
 	if "-" not in w[:]:
 		for i in range(0, len(w)):
@@ -39,7 +42,7 @@ def formation(w, q):
 		str1=w
 	print(str1)
 	str1=""
-	while k<20:
+	while k<15:
 		k=k+1
 		str1=""
 		letter=""
@@ -48,30 +51,32 @@ def formation(w, q):
 			os.system("./input.sh")
 		r=open('./test.hyp','r')					
 		arr=r.read().split(" ")
-			letter=arr[0]
+		letter=arr[0]
 		print(letter)
 		lt=letter.lower()
 		r.close()				
 		try:
-			string=str(check(str1,lt))
+			string=str(check(str1,lt,w))
 						
-		except:
-			print("Not Possible")
+		except Exception as e:
+			print(e)
 			
 		str1=""
 		for j in range(0, len(string)):
 			str1=str1+string[j]+" "
 		print(str1)
-		if string==w or k==20:
+		if string==q or k==15:
 			return string
-		elif int(round(time.time)-start)>=120:
+		elif int(round(time.time())-start)>=180:
 			subprocess.call(["espeak","You lose"])
-			return "End"
+			break
+	return "End"
 
 output = ["" for j in range(4)]
 matrix = [["" for x in range(4)] for y in range(4)]
  
 def display():
+	text=""
 	if output[0]!="":
 		for t in range(4):
 			matrix[int(sys.argv[5])][t]=output[0][t]
@@ -86,26 +91,34 @@ def display():
 			matrix[int(sys.argv[5])+2][t]=output[3][t]
 	for i in range(4):
 		for j in range(4):
-			if matrix[i][j]="":
-				print("-"),
+			if matrix[i][j]=="":
+				text=text+"-"+" "
 			else:
-				print(matrix[i][j]),
-		print()
+				text=text+matrix[i][j]+" "
+		print(text)
+		text=""
 
 n=0
 s=0
-while 1:
-	if len(output[n%4])==0:
+m=0
+z=1
+while z:
+	if len(output[n%4])==0 and output[n%4]!="End":
 		output[n%4]=formation(sys.argv[(n%4)+1], n%4)
+		m=m+1
+		print(m)
 	elif output[n%4]!=sys.argv[(n%4)+1] and output[n%4]!="End":
 		output[n%4]=formation(output[n%4], n%4)
-	elif output[n%4]=="End":
-		break
-	elif output[n%4]==sys.argv[(n%4)+1]:
+		
+	elif output[n%4]==sys.argv[(n%4)+1] and output[n%4]!="End":
 		s=s+1
-	if s>=4:
-		subprocess.call(["espeak","You win"])
-	display()
+		if s>=4:
+			subprocess.call(["espeak","You win"])
+		z=0
+	if output[n%4]=="End":
+		z=0
+	else:
+		display()
 	n=n+1
 	
 subprocess.call(["espeak","-s","125"," Options are 1: Resume and 2: Start another game"])
