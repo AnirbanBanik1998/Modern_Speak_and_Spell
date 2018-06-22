09d3e1d0fa3352ffc8f85def280a3d51c292770d
AnirbanBanik1998 <anirbanbanik17@gmail.com>
2018-06-22 17:31:35 +0530

    Updated Spell_It game structure according to OOP and slightly modified the logic

--- Game/Games/Spell_It!/Spell_It.py
+++ Game/Games/Spell_It!/Spell_It.py
@@ -1,3 +1,6 @@
+'''
+This is the improvised Spell_It game
+'''
 import sys
 sys.path.insert(0, "../../../")
 import random
@@ -6,36 +9,75 @@ import subprocess
 import time
 import os
 from API import recorder, edit
-lock=threading.Lock()
-f= open("../Wordlist.csv",'r')
-rows=f.read().split("\n")
-k=0
-j=0
-print("Spell It!")
-string=""
-str1=""
-answer=""
-my_randoms=random.sample(range(0, len(rows)), 1)
-column=rows[my_randoms[0]].split(",")
-randcolumn=random.sample(range(0, (len(column)-1)), 1)
-column[randcolumn[0]]=column[randcolumn[0]].strip()
-subprocess.call(["espeak","-s","100"," Spell "+column[randcolumn[0]]])
-with lock:
-	record=recorder.Recorder("../../../Language_Models/", LIB_FILE="characters", SILENCE=1, TRIALS=len(column[randcolumn[0]]), DECODE=True, OUTPUT_SHELL="./decoder.sh")
-	record.start()
-r=open("./file.txt", "r+")
-word=r.read().split(" ")
-string = ""
-for i in range(0, len(word)):
-	string=string + word[i]
-if string==column[randcolumn[0]]:
-	subprocess.call(["espeak","-s","150"," Good!"])
 
-else:
-	subprocess.call(["espeak","-s","150"," No you are wrong...the answer will be "])
-	for i in range(0, len(column[randcolumn[0]])):
-		subprocess.call(["espeak","-s","100", column[randcolumn[0]][i]])
-subprocess.call(["espeak","-s","125"," Options are 1: Resume and 2: Start another game"])
-os.system("truncate -s 0 file.txt")
-f.close()
-r.close()
+class Spell:
+	def __init__(self):
+		self.lock=threading.Lock()
+		self.f= open("../Wordlist.csv",'r')
+		self.rows=self.f.read().split("\n")
+		self.f.close()
+		self.k=0
+		self.j=0
+		print("Spell It!")
+		
+		self.counter=0
+		self.flag=0
+		self.string=""
+		self.str1=""
+		self.answer=""
+		
+	def rand(self):
+		'''
+		Generates a random word from Wordlist.csv
+		'''
+		self.my_randoms=random.sample(range(0, len(self.rows)), 1)
+		self.column=self.rows[self.my_randoms[0]].split(",")
+		self.randcolumn=random.sample(range(0, (len(self.column)-1)), 1)
+		self.column[self.randcolumn[0]]=self.column[self.randcolumn[0]].strip()
+		subprocess.call(["espeak","-s","100"," Spell "+self.column[self.randcolumn[0]]])
+		return self.column[self.randcolumn[0]]
+		
+	def test(self, string, w):
+		'''
+		Function to take the recording and check if it matches with the letters.
+		
+		:param string: The string to be formed as the result.
+		:param w: The specific letter to be checked.
+		
+		:return: Returns string, improvised by the function itself.
+		'''
+		self.counter+=1
+		with self.lock:
+			record=recorder.Recorder("../../../Language_Models/", SILENCE=1, TRIALS=1, DECODE=True, LIB_FILE="characters", OUTPUT_SHELL="./decoder.sh")
+			record.start()
+		f=open("./file.txt", "r")
+		letter=f.read().strip()
+		if letter.lower()==w:
+			string=string+w
+			return string
+		else:
+			return "-"
+		
+	def terminal(self, word):
+		'''
+		Main function to take care of the whole process
+		
+		:param word: Takes the randomly generated word from rand() as input parameter.
+		'''
+		w=""
+		i=0
+		while i<len(word) and self.counter<=20:
+			w1=self.test(w, word[i])
+			if w1 is not "-":
+				w=w1
+				print(w)
+			else:
+				print(w+w1)
+			if w==word:
+				subprocess.call(["espeak","-s","125"," Good!"])
+				break
+			if self.counter>20:
+				subprocess.call(["espeak","-s","125"," No you are wrong...the answer will be "])
+				for j in word:
+					subprocess.call(["espeak","-s","100", j])
+		subprocess.call(["espeak","-s","125"," Options are 1: Resume and 2: Start another game"])
