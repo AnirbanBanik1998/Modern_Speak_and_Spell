3b84b0684b21af647474cfd66a97b24b781fc922
AnirbanBanik1998 <anirbanbanik17@gmail.com>
2018-06-24 16:30:50 +0530

    Updated live_Speech_Processing

--- Speech_Processing/accuracy_check/rename.py
+++ Speech_Processing/accuracy_check/rename.py
@@ -1,8 +1,17 @@
 import os
 f=open("test.transcription", "r")
 letter=f.read().split(" ")
+r=open("test.hyp","r")
+hyp=r.read().split(" ")
+r.close()
+result=""
+for r in hyp[:len(hyp)-2]:
+	result=result+r+" "
+result=result.strip()
 for l in letter:
-	if ord(l) in range(65,91):
-		os.system("mv test.hyp "+l+".dic")
+	if l is not '' and ord(l) in range(65,91):
+		with open((l+".dic"), "a+") as w:
+			w.write(result+"\n")
+		os.remove("test.hyp")
 		break
 f.close()
--- Speech_Processing/accuracy_check/run.py
+++ Speech_Processing/accuracy_check/run.py
@@ -1,7 +1,8 @@
 import sys
 sys.path.insert(0, "../../")
 from API import recorder, edit
-
-record=recorder.Recorder("../../Language_Models/", DECODE=True, TRANSCRIBE=True, OUTPUT_SHELL="./lock.sh")
-record.start()
-
+import threading
+lock=threading.Lock()
+with lock:
+	record=recorder.Recorder("../../Language_Models/", DECODE=True, TRANSCRIBE=True, OUTPUT_SHELL="./lock.sh")
+	record.start()
