60326297b79bd1bd44fa40548af689b7e649ef99
AnirbanBanik1998 <anirbanbanik17@gmail.com>
2018-06-26 21:35:25 +0530

    Updated structure of Hangman according to OOP principles

--- Game/Games/Hangman/Hangman.py
+++ Game/Games/Hangman/Hangman.py
@@ -6,64 +6,82 @@ import time
 import os
 import threading
 from API import recorder, edit
-lock=threading.Lock()
-f= open("../Wordlist.csv",'r')
-rows=f.read().split("\n")
-k=0
-print("Enter one letter in a single try:")
-string=""
-str1=""
-my_randoms=random.sample(range(0, len(rows)), 1)
-column=rows[my_randoms[0]].split(",")
-randcolumn = random.sample(range(0, (len(column) - 1)), 1)
-column[randcolumn[0]]=column[randcolumn[0]].strip()
-for i in range(0, len(column[randcolumn[0]])):
-	string=string+"-"
-for i in range(0,len(string)):
-	str1=str1+string[i]+" "
-print (str1)
+
+class Hangman:
+	def __init__(self):
+		self.lock=threading.Lock()
+		self.f= open("../Wordlist.csv",'r')
+		self.rows=self.f.read().split("\n")
+		self.f.close()
+		self.k=0
+		self.string=""
+		self.str1=""
+		self.counter=0
 		
-str1=""
-def check(str1,l):
-	print(" Input is "+l)
-	for j in range(0, len(string)):
-		if l==column[randcolumn[0]][j] and string[j]=="-":
-			str1=str1+l
-		else:
-			str1=str1+string[j]
-	return str1
-while k<20:
-	k=k+1
-	str1=""
-	letter=""
-	print("Enter letter: ")
-	with lock:
-		record=recorder.Recorder("../../../Language_Models/", LIB_FILE="characters", SILENCE=1, TRIALS=1, DECODE=True)
-		record.start()
-	r=open('./test.hyp','r')					
-	arr=r.read().split(" ")
-	letter=arr[0]
-	print(letter)
-	lt=letter.lower()
-	r.close()				
-	try:
-		string=str(check(str1,lt))
-						
-	except:
-		print("Not Possible")
-			
-	str1=""
-	for j in range(0, len(string)):
-		str1=str1+string[j]+" "
-	print(str1)
-	if string==column[randcolumn[0]]:
-		subprocess.call(["espeak","You win"])
-		break
-	elif k==20:
-		subprocess.call(["espeak","You lose"])
-		with lock:
-			subprocess.call(["espeak","The answer is "+column[randcolumn[0]]])
-		print(" Answer is: "+column[randcolumn[0]])
+	def rand(self):
+		'''
+		Generates a random word from Wordlist.csv
+		'''
+		self.my_randoms=random.sample(range(0, len(self.rows)), 1)
+		self.column=self.rows[self.my_randoms[0]].split(",")
+		self.randcolumn=random.sample(range(0, (len(self.column)-1)), 1)
+		self.column[self.randcolumn[0]]=self.column[self.randcolumn[0]].strip()
+		return self.column[self.randcolumn[0]]
 	
-subprocess.call(["espeak","-s","125"," Options are 1: Resume and 2: Start another game"])
-f.close()
+	def initialize(self, r):
+		str1=""
+		for i in range(0, len(r)):
+			self.string=self.string+"-"
+		for i in range(0,len(self.string)):
+			str1=str1+self.string[i]+" "
+		return(str1)
+		
+	def check(self, str1, l, r):
+		print(" Input is "+l)
+		for j in range(0, len(self.string)):
+			if l==r[j] and self.string[j]=="-":
+				str1=str1+l
+			else:
+				str1=str1+self.string[j]
+		return str1
+		
+	def terminal(self, word):
+		self.str1=self.initialize(word)
+		print(self.str1)
+		self.str1=""
+		while self.counter<20:
+			self.counter=self.counter+1
+			self.str1=""
+			letter=""
+			print("Enter letter: ")
+			with self.lock:
+				record=recorder.Recorder("../../../Language_Models/", LIB_FILE="characters", SILENCE=1, TRIALS=1, DECODE=True)
+				record.start()
+			r=open('./test.hyp','r')					
+			arr=r.read().split(" ")
+			letter=arr[0]
+			print(letter)
+			lt=letter.lower()
+			r.close()				
+			try:
+				self.string=str(self.check(self.str1,lt, word))
+						
+			except Exception as e:
+				print(e)
+			
+			self.str1=""
+			for j in range(0, len(self.string)):
+				self.str1=self.str1+self.string[j]+" "
+			print(self.str1)
+			if self.string==word:
+				subprocess.call(["espeak","You win"])
+				break
+			elif self.counter==20:
+				subprocess.call(["espeak","You lose"])
+				with self.lock:
+					subprocess.call(["espeak","The answer is "+word])
+				print(" Answer is: "+word)
+				
+obj=Hangman()
+random=obj.rand()
+obj.terminal(random)
