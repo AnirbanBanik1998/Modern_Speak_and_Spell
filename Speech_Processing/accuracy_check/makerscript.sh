f099cd2cbc4abfd05a06353585fb5cf3f85fc5df
AnirbanBanik1998 <anirbanbanik17@gmail.com>
2018-06-26 18:44:55 +0530

    Updated shell scripts according to the improvements done

--- Speech_Processing/accuracy_check/makerscript.sh
+++ Speech_Processing/accuracy_check/makerscript.sh
@@ -1,13 +1,14 @@
 python3 final.py
 if ! [ -e test.hyp ]
 then
-	if [ -e phonetic.csv ]
+	if ! [ -e phonetic.csv ]
 	then 
-		python3 add.py
-	else
 		python3 make.py
-		python3 add.py
+	else
+		echo "phonetic csv file present"
 	fi
+	python3 add.py
+	python3 modify.py
 else
 	echo "Test hypothesis file is present"
 fi
--- Speech_Processing/accuracy_check/sample_audio_processing.sh
+++ Speech_Processing/accuracy_check/sample_audio_processing.sh
@@ -9,14 +9,14 @@ python3 dictionary.py
 python3 translate.py
 python3 final.py
 
-if [ -e	phonetic.csv ]
+if ! [ -e phonetic.csv ]
 then
-	python3 add.py
-else 
 	python3 make.py
-	python3 add.py
+else
+	echo "phonetic csv file present"
 fi
-
+python3 add.py
+python3 modify.py
 rm generate.py
 rm voices.json
 rm a.txt
