8bfa7e441040e233243374b677dbb0fbd3e8374a
AnirbanBanik1998 <anirbanbanik17@gmail.com>
2018-07-29 23:37:33 +0530

    Updated setup.sh with root-access checking facility to install all the required dependencies

--- setup.sh
+++ setup.sh
@@ -1,29 +1,35 @@
 #!/bin/sh
 # This script is mainly to install and update all the dependencies for this project.
 
+# Checking for root access
+if [ "$EUID" -ne 0 ]; then
+    echo "Requires root to install dependencies. Use : sudo setup.sh"
+    exit 1
+fi
+
 # Install espeak
-sudo apt-get install espeak
+apt-get install -y espeak
 
 # Install Sphinxtrain
-sudo apt-get update
-sudo apt-get install sphinxtrain
+apt-get update
+apt-get install -y sphinxtrain
 
 # Installing pyaudio
-sudo apt-get install libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0
-sudo apt-get install ffmpeg libav-tools
-sudo pip install pyaudio
+apt-get install -y libasound-dev portaudio19-dev libportaudio2 libportaudiocpp0
+apt-get install -y ffmpeg libav-tools
+pip install pyaudio
 
 # Installing Wave
-sudo pip install Wave
+pip install Wave
 
 # Installing pocketsphinx
-sudo apt-get install swig
-sudo pip install --upgrade pip setuptools wheel
-sudo apt-get install libpulse-dev
-sudo pip install --upgrade pocketsphinx || sudo pip --no-cache-dir install --upgrade pocketsphinx
+apt-get install -y swig
+pip install --upgrade pip setuptools wheel
+apt-get install -y libpulse-dev
+pip install --upgrade pocketsphinx || sudo pip --no-cache-dir install --upgrade pocketsphinx
 
 # Installing pygame
 python3 -m pip install -U pygame --user
 
 # Installing PyDictionary
-sudo pip install PyDictionary
+pip install PyDictionary
